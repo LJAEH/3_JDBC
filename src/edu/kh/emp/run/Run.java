b1888561339ce5d8cb162b0c435e496fcb444a90
LJAEH <wogurdlek33@gmail.com>
2023-02-03 09:02:02 +0900

    JDBC

---
+++ src/edu/kh/emp/run/Run.java
@@ -0,0 +1,13 @@
+package edu.kh.emp.run;
+
+import edu.kh.emp.view.EmployeeView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new EmployeeView().displayMenu();
+	}
+
+}
